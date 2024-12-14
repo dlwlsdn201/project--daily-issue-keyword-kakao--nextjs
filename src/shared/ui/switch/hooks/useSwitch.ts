67ab77cc93de0ae9902bd252a3133a8eff3d5c20
JinWoo_Lee <dlwlsdn201@naver.com>
2024-12-14 18:14:39 +0900

    :sparkles: #1 switch UI 컴포넌트의 커스텀 훅 코드 신규 작성

--- src/shared/ui/switch/hooks/useSwitch.ts
+++ src/shared/ui/switch/hooks/useSwitch.ts
@@ -1,15 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface Props {
   initialValue: boolean;
+  onToggle?: (value: boolean) => void;
 }
 
-export const useSwitch = ({ initialValue }: Props) => {
+export const useSwitch = ({ initialValue, onToggle }: Props) => {
   const [checked, setChecked] = useState(initialValue);
 
   const toggleSwitch = (nextChecked: boolean) => {
     setChecked(nextChecked);
   };
+
+  useEffect(() => {
+    if (onToggle) {
+      onToggle(checked);
+    }
+  }, [checked]);
 
   return { checked, toggleSwitch };
 };
